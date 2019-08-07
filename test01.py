1eb0e879092596d05c8ef6650fbf82de79334def
d8Atman <53858095+d8Atman@users.noreply.github.com>
2019-08-07 11:19:54 +0200

    Add files via upload

---
+++ test01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 20:59:55 2019
+
+@author: diego
+
+Perro 
+"""
+print(" Olaf      /|")
+print(" ist      / |")
+print(" eine    /  |")
+print(" gute /___|")
+print(" Agente     |")
+print(" :D         |")
+
